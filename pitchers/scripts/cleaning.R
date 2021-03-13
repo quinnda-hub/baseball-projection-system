b38abf4be8799de6709ea07e212f07125eef24c5
quinnda-hub <79235264+quinnda-hub@users.noreply.github.com>
2021-03-13 11:36:01 -0600

    created records for all consec yrs

--- pitchers/scripts/cleaning.R
+++ pitchers/scripts/cleaning.R
@@ -29,7 +29,7 @@ con_yrs <- function(df, num_years, multiple) {
 }
 
 # get our list of players to regress later on
-years <- c(5, 10, 15, 20)
+years <- seq(5, 20, 5)
 pitchers_1 <- lapply(years, con_yrs, df = master, multiple = 5)
 
 pitchers_1 <- rbindlist(pitchers_1)
@@ -37,10 +37,33 @@ setorder(pitchers_1, name) # order by name recommended to simplify future steps
 
 # write.csv(pitchers_1, "pitchers-5-yrs.csv")
 
-years <- c(4, 8, 12, 16)
+years <- seq(4, 20, 4)
 pitchers_2 <- lapply(years, con_yrs, df = master, multiple = 4)
 
 pitchers_2 <- rbindlist(pitchers_2)
 setorder(pitchers_2, name)
 
-# write.csv(pitchers_2, "pitchers-4-yrs.csv)
+# write.csv(pitchers_2, "pitchers-4-yrs.csv")
+
+years <- seq(3, 20, 3)
+pitchers_3 <- lapply(years, con_yrs, df = master, multiple = 3)
+
+pitchers_3 <- rbindlist(pitchers_3)
+setorder(pitchers_3, name)
+
+write.csv(pitchers_3, "pitchers-3-yrs.csv")
+
+years <- seq(2, 20, 2)
+pitchers_4 <- lapply(years, con_yrs, df = master, multiple = 2)
+
+pitchers_4 <- rbindlist(pitchers_4)
+setorder(pitchers_4, name)
+
+write.csv(pitchers_4, "pitchers-3-yrs.csv")
+
+years <- seq(1:20)
+pitchers_5 <- lapply(years, con_yrs, df = master, multiple = 1)
+
+pitchers_5<- rbindlist(pitchers_5)
+setorder(pitchers_5, name)
+write.csv(pitchers_5, "pitchers-1-yrs.csv")
